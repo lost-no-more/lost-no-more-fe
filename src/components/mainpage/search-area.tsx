74961986160cd9664ffbf607b10ba68f44203230
milkbottle0305 <bigtog0602@gmail.com>
2025-02-05 22:20:12 +0900

    feat: 검색영역에 검색필터가 동적으로 변경되도록 구현

--- src/components/mainpage/search-area.tsx
+++ src/components/mainpage/search-area.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import IconInput from '@/components/common/icon-input';
 import SearchIcon from '../icons/search-icon';
 import {
@@ -8,10 +10,15 @@ import {
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import LocationIcon from '../icons/location-icon';
-import { LostLocations, LostCategories } from '@/types/lost-property';
+import { LostLocations, LostCategories, LostLocation, LostCategory } from '@/types/lost-property';
 import CategoryIcon from '../icons/category-icon';
+import { useState } from 'react';
 
 export default function SearchArea() {
+  const [keyword, setKeyword] = useState<string>('');
+  const [location, setLocation] = useState<LostLocation | null>(null);
+  const [category, setCategory] = useState<LostCategory | null>(null);
+
   return (
     <>
       <IconInput
@@ -22,6 +29,8 @@ export default function SearchArea() {
             <input
               className="w-full bg-background outline-none"
               type="text"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
               placeholder={'무엇을 잃어버리셨나요?'}
             />
           ),
@@ -29,39 +38,47 @@ export default function SearchArea() {
       />
       <div className="flex items-center gap-6">
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger className="w-full">
             <IconInput
               slots={{
                 icon: () => (
                   <LocationIcon width={31.61} height={28} fill="hsl(var(--secondary-foreground))" />
                 ),
-                input: () => <p className="w-full bg-background">지역</p>,
+                input: () => (
+                  <p className="w-full bg-background text-start">{location || '지역'}</p>
+                ),
               }}
             />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuGroup>
+            <DropdownMenuGroup className="max-h-96 overflow-y-auto">
               {LostLocations.map((location) => (
-                <DropdownMenuItem key={location}>{location}</DropdownMenuItem>
+                <DropdownMenuItem key={location} onClick={() => setLocation(location)}>
+                  {location}
+                </DropdownMenuItem>
               ))}
             </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger className="w-full">
             <IconInput
               slots={{
                 icon: () => (
                   <CategoryIcon width={31.61} height={28} fill="hsl(var(--secondary-foreground))" />
                 ),
-                input: () => <p className="w-full bg-background">상품 카테고리</p>,
+                input: () => (
+                  <p className="w-full bg-background text-start">{category || '상품 카테고리'}</p>
+                ),
               }}
             />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuGroup>
+            <DropdownMenuGroup className="max-h-96 overflow-y-auto">
               {LostCategories.map((category) => (
-                <DropdownMenuItem key={category}>{category}</DropdownMenuItem>
+                <DropdownMenuItem key={category} onClick={() => setCategory(category)}>
+                  {category}
+                </DropdownMenuItem>
               ))}
             </DropdownMenuGroup>
           </DropdownMenuContent>
