76a9a89c1aa6d918d89fba6b855432e77221306c
milkbottle0305 <bigtog0602@gmail.com>
2025-02-06 14:41:45 +0900

    feat: 카카오맵 API 연동

---
+++ src/components/searchpage/lost-no-more-map.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { Map } from 'react-kakao-maps-sdk';
+
+export default function LostNoMoreMap() {
+  return <Map center={{ lat: 33.5563, lng: 126.79581 }} className="h-full w-full"></Map>;
+}
