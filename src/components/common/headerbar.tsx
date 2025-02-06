efdde757229b0a08d1e6bf6d0c0c703dc6dd0936
milkbottle0305 <bigtog0602@gmail.com>
2025-02-06 13:27:26 +0900

    feat: 공통 검색바 구현

---
+++ src/components/common/headerbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import LogoIcon from '../icons/logo-icon';
+import NotificationPopover from './notification-popover';
+import Searchbar from './searchbar';
+import UserButton from './user-button';
+
+export default function Headerbar() {
+  return (
+    <div className="flex w-full items-center justify-between bg-primary px-10 py-5">
+      <Link href="/">
+        <LogoIcon width={50} height={50} fill="hsl(var(--background))" />
+      </Link>
+      <Searchbar />
+      <div className="flex items-center gap-4">
+        <NotificationPopover />
+        <UserButton />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/common/searchbar.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { SearchIcon } from 'lucide-react';
+import { useState, useRef } from 'react';
+
+export default function Searchbar() {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  return (
+    <div
+      className={`flex w-96 items-center gap-2 rounded-lg bg-background px-3 py-2.5 ${isFocused ? 'outline outline-2 outline-foreground' : ''}`}
+    >
+      <SearchIcon size={20} color="hsl(var(--primary))" />
+      <input
+        ref={inputRef}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className="w-full text-start text-base focus:outline-none"
+        placeholder="분실물 검색"
+      />
+      <div className="flex items-center justify-center gap-6"></div>
+    </div>
+  );
+}
