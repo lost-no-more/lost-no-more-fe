7ca1d04eaaf263496b88ac23a9a264907d7ec240
milkbottle0305 <bigtog0602@gmail.com>
2025-02-06 16:42:02 +0900

    feat: 줌컨트롤 및 내 위치 이동 버튼 구현

--- src/components/searchpage/lost-no-more-map.tsx
+++ src/components/searchpage/lost-no-more-map.tsx
@@ -1,6 +1,39 @@
 'use client';
 import { Map } from 'react-kakao-maps-sdk';
+import MoveMyPosButton from './move-mypos-button';
+import { useLostNoMoreMapContext } from '@/contexts/lost-no-more-map-context';
+import { useMemo } from 'react';
+import { debounce } from 'lodash';
+import ZoomController from './zoom-controller';
+
+const MAP_REFRESH_DELAY = 500;
 
 export default function LostNoMoreMap() {
-  return <Map center={{ lat: 33.5563, lng: 126.79581 }} className="h-full w-full"></Map>;
+  const { center, setCenter, level } = useLostNoMoreMapContext();
+  const handleCenterChanged = useMemo(
+    () =>
+      debounce((map: kakao.maps.Map) => {
+        setCenter({
+          lat: map.getCenter().getLat(),
+          lng: map.getCenter().getLng(),
+        });
+      }, MAP_REFRESH_DELAY),
+    [setCenter]
+  );
+
+  return (
+    <Map
+      center={center}
+      level={level}
+      isPanto={true}
+      zoomable={false}
+      onCenterChanged={handleCenterChanged}
+      className="relative h-full w-full"
+    >
+      <div className="absolute bottom-10 left-10 z-10 flex items-center gap-2">
+        <MoveMyPosButton />
+        <ZoomController />
+      </div>
+    </Map>
+  );
 }
