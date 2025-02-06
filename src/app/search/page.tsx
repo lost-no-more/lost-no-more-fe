4bdf77d739b20799eb06f06886987bbcbfbdda35
milkbottle0305 <bigtog0602@gmail.com>
2025-02-06 16:26:17 +0900

    feat: 분실물 지도를 위해 지도의 중심 및 줌인 상태를 관리하는 context 구현

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,14 @@
 import Headerbar from '@/components/common/headerbar';
 import LostNoMoreMap from '@/components/searchpage/lost-no-more-map';
+import { LostNoMoreMapProvider } from '@/contexts/lost-no-more-map-context';
 
 export default function SearchPage() {
   return (
     <div className="flex h-screen w-full flex-col">
       <Headerbar />
-      <LostNoMoreMap />
+      <LostNoMoreMapProvider>
+        <LostNoMoreMap />
+      </LostNoMoreMapProvider>
     </div>
   );
 }
