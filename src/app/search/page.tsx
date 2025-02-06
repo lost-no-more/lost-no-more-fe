cf93b4e0146a993c31118160f5617e2dccef6ebf
milkbottle0305 <bigtog0602@gmail.com>
2025-02-06 14:41:45 +0900

    feat: 카카오맵 API 연동

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,11 @@
 import Headerbar from '@/components/common/headerbar';
+import LostNoMoreMap from '@/components/searchpage/lost-no-more-map';
 
 export default function SearchPage() {
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex h-screen w-full flex-col">
       <Headerbar />
+      <LostNoMoreMap />
     </div>
   );
 }
