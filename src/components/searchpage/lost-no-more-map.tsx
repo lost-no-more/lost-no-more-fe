1553fcf811a905cb4b79ed6d3790da5e52b73e0b
milkbottle0305 <bigtog0602@gmail.com>
2025-02-07 14:40:30 +0900

    feat: 랜덤 목업 데이터를 마커로 노출한다

--- src/components/searchpage/lost-no-more-map.tsx
+++ src/components/searchpage/lost-no-more-map.tsx
@@ -1,15 +1,18 @@
 'use client';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, Circle, MapMarker } from 'react-kakao-maps-sdk';
 import MoveMyPosButton from './move-mypos-button';
 import { useLostNoMoreMapContext } from '@/contexts/lost-no-more-map-context';
-import { useMemo } from 'react';
+import { useMemo, useRef, useCallback, useState } from 'react';
 import { debounce } from 'lodash';
 import ZoomController from './zoom-controller';
+import { LOSTITEMS_LOCATION } from '@/constants/lost-items';
 
 const MAP_REFRESH_DELAY = 500;
 
 export default function LostNoMoreMap() {
   const { center, setCenter, level } = useLostNoMoreMapContext();
+  const mapRef = useRef<kakao.maps.Map>(null);
+
   const handleCenterChanged = useMemo(
     () =>
       debounce((map: kakao.maps.Map) => {
@@ -21,6 +24,37 @@ export default function LostNoMoreMap() {
     [setCenter]
   );
 
+  const getVisibleItems = useCallback(() => {
+    if (!mapRef.current) return [];
+
+    const bounds = mapRef.current.getBounds();
+    const sw = bounds.getSouthWest();
+    const ne = bounds.getNorthEast();
+
+    console.log(sw.getLat(), ne.getLat(), sw.getLng(), ne.getLng());
+    console.log(LOSTITEMS_LOCATION[0]);
+    const visibleItems = LOSTITEMS_LOCATION.filter(
+      (item) =>
+        item.latitude >= sw.getLat() &&
+        item.latitude <= ne.getLat() &&
+        item.longitude >= sw.getLng() &&
+        item.longitude <= ne.getLng()
+    );
+    console.log(visibleItems);
+
+    return visibleItems;
+  }, []);
+
+  const [visibleItems, setVisibleItems] = useState<typeof LOSTITEMS_LOCATION>([]);
+
+  const handleBoundsChanged = useMemo(
+    () =>
+      debounce(() => {
+        setVisibleItems(getVisibleItems());
+      }, MAP_REFRESH_DELAY),
+    [getVisibleItems]
+  );
+
   return (
     <Map
       center={center}
@@ -28,8 +62,13 @@ export default function LostNoMoreMap() {
       isPanto={true}
       zoomable={false}
       onCenterChanged={handleCenterChanged}
+      onBoundsChanged={handleBoundsChanged}
+      onCreate={(map) => (mapRef.current = map)}
       className="relative h-full w-full"
     >
+      {visibleItems.map((item, index) => (
+        <MapMarker key={index} position={{ lat: item.latitude, lng: item.longitude }} />
+      ))}
       <div className="absolute bottom-10 left-10 z-10 flex items-center gap-2">
         <MoveMyPosButton />
         <ZoomController />
