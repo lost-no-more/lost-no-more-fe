fe1b6eb7dd68d5dae354c6f25e32f4f5f760e177
milkbottle0305 <bigtog0602@gmail.com>
2025-02-10 21:16:20 +0900

    fix: 분실물카드 디자인 수정(truncate, 카테고리 안보이는 현상)

--- src/components/common/lost-card.tsx
+++ src/components/common/lost-card.tsx
@@ -16,8 +16,8 @@ export default function LostCard({
   acquisitionDate,
 }: LostCardProps) {
   return (
-    <div className="relative box-content flex w-[270px] shrink-0 cursor-pointer flex-col overflow-hidden rounded-xl border-2 border-solid border-border">
-      <div className="absolute right-3 top-3 rounded-md bg-foreground px-2.5 py-1.5 text-xs font-semibold text-background">
+    <div className="relative box-border flex w-[274px] shrink-0 cursor-pointer flex-col overflow-hidden rounded-xl border-2 border-solid border-border">
+      <div className="absolute right-3 top-3 z-10 rounded-md bg-primary px-2.5 py-1.5 text-xs font-semibold text-background">
         {category}
       </div>
       <div
@@ -32,47 +32,15 @@ export default function LostCard({
           className="object-cover"
         />
       </div>
-      <div
-        data-cid="div-tTENZd"
-        className="flex flex-col border-t-2 border-solid border-border bg-background p-3"
-      >
-        <h1
-          data-cid="h1-A5kABB"
-          className="mb-3 text-2xl font-extrabold text-foreground"
-        >
-          {name}
-        </h1>
-        <div
-          data-cid="div-LQMbVx"
-          className="mb-1 flex items-center gap-2"
-        >
-          <MapPinIcon
-            data-cid="MapPinIcon-4tpp5K"
-            size={16}
-            color="hsl(var(--muted-foreground))"
-          />
-          <p
-            data-cid="p-lzGfHB"
-            className="text-base text-muted-foreground"
-          >
-            {location}
-          </p>
+      <div className="flex w-full flex-col border-t-2 border-solid border-border bg-background p-3">
+        <h1 className="mb-3 truncate text-2xl font-extrabold text-foreground">{name}</h1>
+        <div className="mb-1 flex items-center gap-2">
+          <MapPinIcon size={16} color="hsl(var(--muted-foreground))" />
+          <p className="truncate text-base text-muted-foreground">{location}</p>
         </div>
-        <div
-          data-cid="div-mcDpw7"
-          className="flex items-center gap-2"
-        >
-          <Clock10Icon
-            data-cid="Clock10Icon-QjnF6N"
-            size={16}
-            color="hsl(var(--muted-foreground))"
-          />
-          <p
-            data-cid="p-KLFAFk"
-            className="text-base text-muted-foreground"
-          >
-            습득일자: {acquisitionDate}
-          </p>
+        <div className="flex items-center gap-2">
+          <Clock10Icon size={16} color="hsl(var(--muted-foreground))" />
+          <p className="truncate text-base text-muted-foreground">습득일자: {acquisitionDate}</p>
         </div>
       </div>
     </div>
