789f1bc37b650a281752975ce58c89ba41b74821
milkbottle0305 <bigtog0602@gmail.com>
2025-02-12 15:39:37 +0900

    feat: LostCard id 누락하는 타입 에러 해결

--- src/components/mainpage/mainpage-content.tsx
+++ src/components/mainpage/mainpage-content.tsx
@@ -76,6 +76,7 @@ export default function MainpageContent() {
                   <CarouselItem data-cid="CarouselItem-sbMufa" key={index} className="basis-1/3">
                     <LostCard
                       data-cid="LostCard-glOAzq"
+                      id={index}
                       name="습득물 1"
                       image={'https://sitem.ssgcdn.com/26/64/85/item/1000277856426_i1_750.jpg'}
                       category={'지갑'}
