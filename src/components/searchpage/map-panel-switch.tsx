7cbdb724240feb3a15d5bb7f8c99348640f25cc2
milkbottle0305 <bigtog0602@gmail.com>
2025-02-12 09:18:38 +0900

    feat: motion 을 활용하여 패널과 디테일패널 애니메이션 적용

--- src/components/searchpage/map-panel-switch.tsx
+++ src/components/searchpage/map-panel-switch.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { useMapPanelContext } from '@/contexts/map-panel-context';
+import { motion } from 'motion/react';
 import MapDetailPanel from './map-detail-panel';
 import MapPanel from './map-panel';
 
 export default function MapPanelSwitch() {
   const { isPanelOpen } = useMapPanelContext();
 
-  return isPanelOpen ? <MapDetailPanel /> : <MapPanel />;
+  return (
+    <motion.div
+      animate={{ width: isPanelOpen ? 540 : 314 }}
+      transition={{ duration: 0.4, ease: 'easeInOut' }}
+    >
+      {isPanelOpen ? <MapDetailPanel /> : <MapPanel />}
+    </motion.div>
+  );
 }
