d5e92320ee7ed7d220270dd125bd9b95aa74d64a
milkbottle0305 <bigtog0602@gmail.com>
2025-02-21 21:20:33 +0900

    fix: search context에 keyword도 null이 되도록 설정

--- src/domain/search/contexts/search-context.tsx
+++ src/domain/search/contexts/search-context.tsx
@@ -6,19 +6,21 @@ import { useState } from 'react';
 import type { LostCategory, LostLocation } from '@/shared/types/lost-property';
 import { buildContext } from '@/shared/utils/build-context';
 
+/* eslint-disable @typescript-eslint/no-empty-function */
+
 /**
  * SearchContextProps
  * @interface
- * @property {string} keyword - 검색 키워드
- * @property {Dispatch<SetStateAction<string>>} setKeyword - 검색 키워드를 설정하는 함수
- * @property {LostLocation} location - 검색 지역
- * @property {Dispatch<SetStateAction<LostLocation>>} setLocation - 검색 지역을 설정하는 함수
- * @property {LostCategory} category - 검색 카테고리
- * @property {Dispatch<SetStateAction<LostCategory>>} setCategory - 검색 카테고리를 설정하는 함수
+ * @property {string | null} keyword - 검색 키워드
+ * @property {Dispatch<SetStateAction<string | null>>} setKeyword - 검색 키워드를 설정하는 함수
+ * @property {LostLocation | null} location - 검색 지역
+ * @property {Dispatch<SetStateAction<LostLocation | null>>} setLocation - 검색 지역을 설정하는 함수
+ * @property {LostCategory | null} category - 검색 카테고리
+ * @property {Dispatch<SetStateAction<LostCategory | null>>} setCategory - 검색 카테고리를 설정하는 함수
  */
 interface SearchContextProps {
-  keyword: string;
-  setKeyword: Dispatch<SetStateAction<string>>;
+  keyword: string | null;
+  setKeyword: Dispatch<SetStateAction<string | null>>;
   location: LostLocation | null;
   setLocation: Dispatch<SetStateAction<LostLocation | null>>;
   category: LostCategory | null;
@@ -26,7 +28,7 @@ interface SearchContextProps {
 }
 
 const [_SearchContext, useSearchContext] = buildContext<SearchContextProps>('Search', {
-  keyword: '',
+  keyword: null,
   setKeyword: () => {},
   location: null,
   setLocation: () => {},
@@ -35,7 +37,7 @@ const [_SearchContext, useSearchContext] = buildContext<SearchContextProps>('Sea
 });
 
 const SearchProvider = ({ children }: { children: React.ReactNode }) => {
-  const [keyword, setKeyword] = useState<string>('');
+  const [keyword, setKeyword] = useState<string | null>(null);
   const [location, setLocation] = useState<LostLocation | null>(null);
   const [category, setCategory] = useState<LostCategory | null>(null);
 
